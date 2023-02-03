89d0e2be52c41f1803c749447c32ddfb9f2b106c
unknown <joaovictorsilva.dev@gmail.com>
2023-02-03 13:18:55 -0400

    chore: main page added

---
+++ tlou_wiki/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';import Main from './Pages/main';
+;
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ tlou_wiki/src/Pages/main/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+export default function Main(){
+    return(
+        <div>
+            <div id="extern">
+                <div id='content'>
+                    <section>
+                        <h3>Quando estiver na escuridão, procure pela luz.</h3>
+                        <p>Conheça os personagens</p>
+                    </section>
+                    <button>Characters</button>
+                </div>
+            </div>
+        </div>
+    )
+}
